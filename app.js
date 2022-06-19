97cdf5320e54b3320631f455b887c3422ec8b0d6
Yasin Yağmur <yasin.yagmur@gmail.com>
2022-06-19 23:13:22 +0300

    fixed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const hour =document.querySelector('.hour');
 function digitalClock() {
     let dateTime = new Date();
     // console.log(dateTime);
-    let clock = dateTime.toLocaleTimeString();
+    let clock = dateTime.toLocaleTimeString('en-US');
     // console.log(clock);
     hour.innerText = clock;
 }
